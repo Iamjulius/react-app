9f4616a9f663e8cb7f2b0ee2140f37b921f568d8
julius mwita <38252983+msabi@users.noreply.github.com>
2020-06-19 18:06:21 +0530

    redux

---
+++ redux/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, combineReducers } from "redux";
+
+//action creator
+const addItem = item => {
+  return {
+    type: "ADD_ITEM",
+    item: item
+  };
+};
+
+//reducer
+const reducer = (state = [], action) => {
+  switch (action.type) {
+    case "ADD_ITEM":
+      return [...state, action.item];
+    default:
+      return state;
+  }
+};
+
+var store = createStore(reducer);
+
+console.log(store.dispatch(addItem("a")));
+console.log(store.getState());
+console.log(store.dispatch(addItem("b")));
+console.log(store.getState());
+console.log(store.dispatch(addItem("c")));
+console.log(store.getState());
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
