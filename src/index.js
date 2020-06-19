016cc4b8d62b502fe8c26f57df7e7789540f1f08
julius mwita <38252983+msabi@users.noreply.github.com>
2020-06-19 14:43:43 +0530

    NavLinks

--- src/index.js
+++ src/index.js
@@ -162,3 +162,135 @@ const App = () => (
 );
 
 */
+
+/*
+Link
+The Link component is used to provide a clickable link that will change the URL of the page.
+
+We can import the Link component with:
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+const Home = () => <div>Home</div>;
+const About = () => <div>About</div>;
+const Users = ({ match }) => <div>{match.params.id}</div>;
+
+const Info = ({ match }) => {
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link to={match.url + "/phone"}>Phone</Link>
+        </li>
+        <li>
+          <Link to={match.url + "/email"}>Email</Link>
+        </li>
+        <li>
+          <Link to={match.url + "/address"}>Address</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route
+        path={match.url + "/phone"}
+        render={props => <div>Phone: 123 456 7890</div>}
+      />
+      <Route
+        path={match.url + "/email"}
+        render={props => <div>Email: email@email.com</div>}
+      />
+      <Route
+        path={match.url + "/address"}
+        render={props => <div>Address: 123 Address</div>}
+      />
+    </div>
+  );
+};
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/users/abc">Users: abc</Link>
+        </li>
+        <li>
+          <Link to="/info">Info</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/users/:id" component={Users} />
+      <Route path="/info" component={Info} />
+    </div>
+  </BrowserRouter>
+);
+
+render(<App />, document.getElementById("root"));
+
+
+
+
+
+
+*/
+
+/*
+NavLinks
+
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Route, Link, NavLink } from "react-router-dom";
+
+const Home = () => <div>Home</div>;
+const About = () => <div>About</div>;
+const Users = ({ match }) => <div>{match.params.id}</div>;
+
+var style = {
+  color: "red"
+};
+
+const activeEvent = (match, location) => {
+  if (!match) {
+    return false;
+  }
+  //do something
+  console.log("active on : " + match.url);
+};
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <ul>
+        <li>
+          <NavLink exact activeStyle={style} to="/">
+            Home
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeStyle={style} to="/about">
+            About
+          </NavLink>
+        </li>
+        <li>
+          <NavLink activeStyle={style} to="/users/abc">
+            Users: abc
+          </NavLink>
+        </li>
+      </ul>
+      <hr />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/users/:id" component={Users} />
+    </div>
+  </BrowserRouter
+
+
+*/
