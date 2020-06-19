21544e82fa7a1de1e055bcf01eca2daf5128a431
julius mwita <38252983+msabi@users.noreply.github.com>
2020-06-19 16:44:19 +0530

    Redirects
    The Redirect component will navigate the page to a specified location when rendered.
    Redirect components are often used inside Route components. When the Route is matched, there is often logic that triggers the Redirect component. If you directly render a Redirect component outsi

--- src/index.js
+++ src/index.js
@@ -353,3 +353,53 @@ render(<App />, document.getElementById("root"));
 
 
 */
+
+//Redirects
+/*
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Route, Link, Redirect, Switch } from "react-router-dom";
+
+const Home = () => <div>Home</div>;
+const About = () => <div>About</div>;
+const Error = () => <div>Error: Invalid User</div>;
+
+const Users = ({ match }) => {
+  var validUsers = ["abc", "def"];
+  if (!validUsers.includes(match.params.id)) return <Redirect to="/error" />;
+  else return <div>Welcome: {match.params.id}</div>;
+};
+const DefaultRoute = () => <div>Default Route</div>;
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/users/abc">Users: abc</Link>
+        </li>
+        <li>
+          <Link to="/users/invalid user">Users: invalid user</Link>
+        </li>
+      </ul>
+      <hr />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/about" component={About} />
+        <Route path="/users/:id" component={Users} />
+        <Route path="/error" component={Error} />
+        <Route component={DefaultRoute} />
+      </Switch>
+    </div>
+  </BrowserRouter>
+);
+
+render(<App />, document.getElementById("root"));
+
+*/
