dee9a29dc2fdf5cb316f405c6c11c359715ffbe7
julius mwita <38252983+msabi@users.noreply.github.com>
2020-06-19 14:40:27 +0530

    Route Properties and URL Parameters

--- src/index.js
+++ src/index.js
@@ -78,6 +78,87 @@ const App = () => (
 );
 */
 
-
-
 //Route Properties and URL Parameters
+/*
+You can access them through props, just like any other React property:
+
+const About = (props) => {
+  console.log(props.match)
+  // Object {path: "/about", url: "/about", isExact: true, params: Object}
+  console.log(props.location)
+  // Object {pathname: "/about", search: "", hash: "", state: undefined}
+  console.log(props.history)
+  // Object {length: 1, action: "POP", location: Object}
+  return (
+    <div>
+      About
+    </div>
+)}
+
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <Route path="/about" component = {About}/>
+    </BrowserRouter>
+  </div>
+);
+
+
+You can also access them through ES6 destructuring:
+
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <Route path="/about" render={ ({match,location,history}) => { 
+        console.log(match)
+        // Object {path: "/about", url: "/about", isExact: true, params: Object}
+        console.log(location)
+        // Object {pathname: "/about", search: "", hash: "", state: undefined}
+        console.log(history)
+        // Object {length: 1, action: "POP", location: Object}
+        return (
+          <div>About</div>
+        )}
+      }/>
+    </BrowserRouter>
+  </div>
+);
+
+You can access the URL param value by using the match.params object that is passed down to the rendered component:
+
+const User = ({match}) => {
+ return (
+   <div>
+     UserId: {match.params.id} //this will show the id entered in the URL path
+   </div>
+)}
+
+const App = () => (
+ <div>
+   <BrowserRouter>
+     <Route path="/userId/:id" component = {User}/>
+   </BrowserRouter>
+ </div>
+);
+
+
+if you want to add additional Route components within a component rendered from an original Route component do the following:
+
+const Info = ({match}) => {
+  return (
+    <div>
+      <Route path="{match.url + "/phone"}" render={(props) => <div>Phone: 123 456 7890</div>}/> // renders when URL = /info/phone
+      <Route path="{match.url + "/email"}" render={(props) => <div>Email: email@email.com</div>}/> renders when URL = /info/email
+      <Route path="{match.url + "/address"}" render={(props) => <div>Address: 123 Address</div>}/> renders when URL = /info/address
+    </div>
+)}
+
+const App = () => (
+  <div>
+    <BrowserRouter>
+      <Route path="/info" component = {Info}/>
+    </BrowserRouter>
+  </div>
+);
+
+*/
