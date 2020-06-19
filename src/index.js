b413ec7483c4609392dddfe4fe8615726d282878
julius mwita <38252983+msabi@users.noreply.github.com>
2020-06-19 11:23:30 +0530

    BrowserRouter
    BrowserRouter
    The BrowserRouter component is a wrapper component that allows all of the other React Router components to work. The BrowserRouter component can only have one child element. There are other types of Router components but the BrowserRouter component is the main route

--- src/index.js
+++ src/index.js
@@ -27,5 +27,43 @@ const App = () => (
     </div>
   </BrowserRouter>
 );
+render(<App />, document.getElementById("root"));
+
+//BrowserRouter
+//import { BrowserRouter} from "react-router-dom"
+
+/*
+const BasicExample = () => (
+  <BrowserRouter>
+    <div> //A Router can only have one child element, so make sure everything goes in the div
+      //other React Router components go here
+    </div>
+  </BrowserRouter>
+)
+*/
+
+/*
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
+
+const Home = () => <div>Home</div>;
+const About = () => <div>About</div>;
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <Route path="/home" component={Home} />
+      <Route path="/about" render={() => <div>About</div>} />
+      <Route children={() => <div>Always Rendered</div>} />
+    </div>
+  </BrowserRouter>
+);
 
 render(<App />, document.getElementById("root"));
+*/
+
+/*
+<Route exact path="/" render={(props) => <div>Home</div>}/> // "only a URL path of "/" will match
+<Route strict path="/about" render={(props) => <div>About</div>}/> // A URL path of "/about" matches but "/about/" doesn't match
+*/
