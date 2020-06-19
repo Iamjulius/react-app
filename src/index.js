2eebf6da400838a202569a2408fc1ce98e480688
julius mwita <38252983+msabi@users.noreply.github.com>
2020-06-19 16:48:39 +0530

    Prompts
    The Prompt component will send a message to the user if the user ever navigates away from the page.

--- src/index.js
+++ src/index.js
@@ -403,3 +403,42 @@ const App = () => (
 render(<App />, document.getElementById("root"));
 
 */
+
+//Prompts
+
+/*
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Route, Link, Prompt } from "react-router-dom";
+
+const Home = () => <div>Home</div>;
+const About = () => <div>About</div>;
+const Users = ({ match }) => <div>{match.url}</div>;
+
+const App = () => (
+  <BrowserRouter>
+    <div>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/users">Users</Link>
+        </li>
+      </ul>
+      <hr />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/users/:id" component={Users} />
+      <Prompt message="Are you sure you want to leave?" />
+    </div>
+  </BrowserRouter>
+);
+
+render(<App />, document.getElementById("root"));
+
+
+*/
