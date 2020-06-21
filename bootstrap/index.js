7230546a1082bc232c65b133dc4e603aa4b95428
julius mwita <38252983+msabi@users.noreply.github.com>
2020-06-21 10:22:02 +0530

    bootstrap

---
+++ bootstrap/index.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { render } from "react-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+
+/*
+yarn add bootstrap@4.0.0
+yarn add reactstrap@5.0.0
+yarn add react-popper@0.9.2
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+*/
