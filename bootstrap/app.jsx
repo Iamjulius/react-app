34a24ebdec3f60e3616749d59259464a406ef4d9
julius mwita <38252983+msabi@users.noreply.github.com>
2020-06-21 12:40:22 +0530

    d

--- bootstrap/app.jsx
+++ bootstrap/app.jsx
@@ -1,6 +1,8 @@
 import { Navbar, NavbarBrand } from "reactstrap";
+import { DISHES } from "./shared/dishes";
 
 class App extends Component {
+  import { DISHES } from './shared/dishes';
   render() {
     return (
       <div className="App">
@@ -9,12 +11,13 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu dishes={this.state.dishes} />
       </div>
     );
   }
 }
 
 //import Menu from './components/MenuComponent';
-
+//https://reactstrap.github.io/components/form/
 //<Menu />
 //https://www.coursera.org/learn/front-end-react/supplement/996so/exercise-instructions-react-components-part-1
